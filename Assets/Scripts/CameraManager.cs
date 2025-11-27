5b8d0ad2a1484345da0ce95691092f6e1a8a1922
Alexander <xrdharley@gmail.com>
2025-11-27 18:24:54 -0500

    Camera follows the player

--- Assets/Scripts/CameraManager.cs
+++ Assets/Scripts/CameraManager.cs
@@ -4,32 +4,10 @@ using UnityEngine.InputSystem;
 public class CameraManager : MonoBehaviour
 {
     [SerializeField] Transform player;
-    [SerializeField] float mouseSensitivity = 70f;
-    float xRotation = 0f;
-    [SerializeField] Vector3 firstPersonOffset = new Vector3(0f, 0.5f, 0f);
+    [SerializeField] Vector3 offSet = new Vector3(0f, 9.5f, 0f);
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    void LateUpdate()
     {
-        Cursor.lockState = CursorLockMode.Locked;
-        Cursor.visible = false;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-        // This is actually all usless, it is supposed to be top down. 
-        transform.position = player.position + firstPersonOffset;
-        transform.rotation = player.rotation;
-
-        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
-        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
-
-        xRotation -= mouseY;
-        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
-
-        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
-        transform.parent.Rotate(Vector3.up * mouseX);
+        transform.position = player.transform.position + offSet;
     }
 }
