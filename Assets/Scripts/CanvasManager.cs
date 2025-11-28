336f24cbbb06e643e9348dc6fae0b02806c4f6ce
Alexander <xrdharley@gmail.com>
2025-11-27 22:43:30 -0500

    Set up the canvas manager to display health

--- Assets/Scripts/CanvasManager.cs
+++ Assets/Scripts/CanvasManager.cs
@@ -4,11 +4,15 @@ using UnityEngine.UI;
 public class CanvasManager : MonoBehaviour
 {
     [SerializeField] Text scoreText;
+    [SerializeField] Text hpText;
+    PlayerManager playerManager;
+    GameObject player;
     GameManager gameManager;
     GameObject canvas;
 
     void Update()
     {
+        // Score
         if (gameManager == null)
         {
             canvas = GameObject.FindGameObjectWithTag("GameController");
@@ -18,5 +22,16 @@ public class CanvasManager : MonoBehaviour
             }
         }
         scoreText.text = $"Score: {gameManager.score:0}";
+
+        // HP
+        if (playerManager == null)
+        {
+            player = GameObject.FindGameObjectWithTag("Player");
+            if (player != null)
+            {
+                playerManager = player.GetComponent<PlayerManager>();
+            }
+        }
+        hpText.text = $"HP: {playerManager.hp:100}";
     }
 }
